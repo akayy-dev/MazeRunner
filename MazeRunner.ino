3a6094cf50d019cff613e86afc4dd308ab7224da
Ahadu Kebede <ahadukebede@gmail.com>
2025-12-07 15:19:59 -0500

    forward and backward things

--- MazeRunner.ino
+++ MazeRunner.ino
@@ -13,12 +13,6 @@
 volatile int *Wheel1State = new int;
 volatile int *Wheel2State = new int;
 
-void onMotorInterrupt(int *motorState) {
-  *motorState = *motorState + 1;
-  if (*motorState > 700) {
-    *motorState = 0;
-  }
-}
 
 enum Direction {
   FORWARD = 1,
@@ -31,6 +25,7 @@ public:
   int motorPin1;
   int motorPin2;
   int motorSpeed;
+  Direction currentDirection;
   volatile int *wheelState;
 
 
@@ -55,14 +50,37 @@ public:
     if (direction == FORWARD) {
       digitalWrite(this->motorPin1, HIGH);
       digitalWrite(this->motorPin2, LOW);
-      analogWrite(this->motorSpeed, speed);
+      this->currentDirection = FORWARD;
+    }
+
+    if (direction == BACKWARD) {
+      digitalWrite(this->motorPin1, LOW);
+      digitalWrite(this->motorPin2, HIGH);
+      this->currentDirection = BACKWARD;
+    }
+    analogWrite(this->motorSpeed, speed);
+  }
+
+  void onFullRotation() {
+    if (this->currentDirection == FORWARD) {
+      this->spinWheel(BACKWARD, 125);
+    }
+    else if (this->currentDirection == BACKWARD) {
+      this->spinWheel(FORWARD, 125);
     }
   }
 };
 
+void onMotorInterrupt(int *motorState, Wheel *wheel) {
+  *motorState = *motorState + 1;
+  if (*motorState > 700) {
+    *motorState = 0;
+    wheel->onFullRotation();
+  }
+}
 
-Wheel wheel1 = Wheel(ENCODER_A, MOTOR1_PIN1, MOTOR2_PIN2, MOTOR1_SPEED, Wheel1State);
-Wheel wheel2 = Wheel(ENCODER_B, MOTOR1_PIN1, MOTOR2_PIN2, MOTOR2_SPEED, Wheel2State);
+Wheel wheel1 = Wheel(ENCODER_A, MOTOR1_PIN1, MOTOR1_PIN2, MOTOR1_SPEED, Wheel1State);
+Wheel wheel2 = Wheel(ENCODER_B, MOTOR2_PIN1, MOTOR2_PIN2, MOTOR2_SPEED, Wheel2State);
 
 void setup() {
   Serial.begin(9600);
@@ -76,11 +94,11 @@ void setup() {
   *Wheel1State = 1;
 
   attachInterrupt(digitalPinToInterrupt(ENCODER_A), []() {
-    onMotorInterrupt(Wheel1State);
+    onMotorInterrupt(Wheel1State, &wheel1);
   },
                   RISING);
   attachInterrupt(digitalPinToInterrupt(ENCODER_B), []() {
-    onMotorInterrupt(Wheel2State);
+    onMotorInterrupt(Wheel2State, &wheel2);
   },
                   RISING);
 
@@ -89,17 +107,27 @@ void setup() {
   Serial.println("Sent signal");
 }
 
-volatile bool motorADir = false;
+bool goingBackwards = false;
 void loop() {
   /* Serial.print("Wheel 1 State: ");
   Serial.print(*wheel1.wheelState); */
   Serial.print(" Wheel 2 State: ");
-  int state = *wheel2.wheelState;
+  int state = *Wheel2State;
   Serial.print(state);
   Serial.println("");
 
   // full rotation
-  if (state == 0) {
-    Serial.println("Wheel has made one full revolution");
-  }
+  // if (state == 0) {
+  //   if (goingBackwards) {
+  //     Serial.println("Wheel has gone backward a revolution, going forward");
+  //     // wheel1.spinWheel(FORWARD, 255);
+  //     // wheel2.spinWheel(FORWARD, 255);
+  //     goingBackwards = false;
+  //   } else {
+  //     Serial.println("Wheel has gone forward a revolution, going backward");
+  //     wheel1.spinWheel(BACKWARD, 255);
+  //     wheel2.spinWheel(BACKWARD, 255);
+  //     goingBackwards = true;
+  //   }
+  // }
 }
